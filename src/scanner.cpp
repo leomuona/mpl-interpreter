64e3bff71d6fa8c557028ad2023c526c92bb561c
Leo Muona <leo.muona@gmail.com>
2014-03-06 17:45:56 +0200

    status rows has now not-functionality. state table has been constructed.

--- src/scanner.cpp
+++ src/scanner.cpp
@@ -1,5 +1,6 @@
 #include "scanner.hpp"
 #include <cstring>
+#include <cstdio>
 
 namespace mpli {
 
@@ -7,32 +8,66 @@ Scanner::Scanner()
 {
     _curr_state = 0;
 
-    /* ALPHA, DIGIT, PERIOD, COMMA, COLON, SEMICOLON, BRACKET_RIGHT,
-        BRACKET_LEFT, PLUS, MINUS, SLASH, BACKSLASH, EXCLAMATIONM,
-        ASTERISK, AND, QUOTEM, EQUALS, SPACE, TAB, NEWLINE */
-
-    /* constructing states table */
+    /* Constructing states table:
+       Must be constructed in a priority order high-low.*/
     /* 0: initial state */
     std::vector<StateRow> v0;
-    v0.push_back(StateRow(ALPHA, 1));
-    v0.push_back(StateRow(DIGIT, 2));
-    v0.push_back(StateRow(PERIOD, 3));
-    v0.push_back(StateRow(COLON, 4));
-    v0.push_back(StateRow(SEMICOLON, 5));
-    v0.push_back(StateRow(BRACKET_RIGHT, 6));
-    v0.push_back(StateRow(BRACKET_LEFT, 7));
-    v0.push_back(StateRow(PLUS, 8));
-    v0.push_back(StateRow(MINUS, 9));
-    v0.push_back(StateRow(SLASH, 10));
-    v0.push_back(StateRow(BACKSLASH, 11));
-    v0.push_back(StateRow(EXCLAMATIONM, 12));
-    v0.push_back(StateRow(ASTERISK, 13));
-    v0.push_back(StateRow(AND, 14));
-    v0.push_back(StateRow(QUOTEM, 15));
-    v0.push_back(StateRow(EQUALS, 16));
+    v0.push_back(StateRow(ALPHA, 10));
+    v0.push_back(StateRow(DIGIT, 20));
+    v0.push_back(StateRow(PERIOD, 30));
+    v0.push_back(StateRow(COMMA, _TOKEN_END_STATE));
+    v0.push_back(StateRow(COLON, 40));
+    v0.push_back(StateRow(SEMICOLON, _TOKEN_END_STATE));
+    v0.push_back(StateRow(BRACKET_RIGHT, _TOKEN_END_STATE));
+    v0.push_back(StateRow(BRACKET_LEFT, _TOKEN_END_STATE));
+    v0.push_back(StateRow(PLUS, _TOKEN_END_STATE));
+    v0.push_back(StateRow(MINUS, _TOKEN_END_STATE));
+    v0.push_back(StateRow(SLASH, _TOKEN_END_STATE));
+    v0.push_back(StateRow(EXCLAMATIONM, _TOKEN_END_STATE));
+    v0.push_back(StateRow(ASTERISK, _TOKEN_END_STATE));
+    v0.push_back(StateRow(AND, _TOKEN_END_STATE));
+    v0.push_back(StateRow(QUOTEM, 50));
+    v0.push_back(StateRow(EQUALS, _TOKEN_END_STATE));
     _states_vec.push_back(State(0, v0));
 
-    // TODO: set certain next-states to -1 or -2
+    /* 10: aplha first: indentifier or keyword */
+    std::vector<StateRow> v10;
+    v10.push_back(StateRow(ALPHA, 10));
+    v10.push_back(StateRow(DIGIT, 10));
+    v10.push_back(StateRow(UNDERSCORE, 10));
+    v10.push_back(StateRow(NOT_ANY, _TOKEN_BREAK_STATE, 1));
+    _states_vec.push_back(State(10, v10));
+
+    /* 20: digit first: int value */
+    std::vector<StateRow> v20;
+    v20.push_back(StateRow(DIGIT, 20));
+    v20.push_back(StateRow(DIGIT, _TOKEN_BREAK_STATE, 1));
+    _states_vec.push_back(State(20, v20));
+
+    /* 30-31: period first: we are looking for .. operator */
+    std::vector<StateRow> v30;
+    v30.push_back(StateRow(PERIOD, 31));
+    _states_vec.push_back(State(30, v30));
+    std::vector<StateRow> v31;
+    v31.push_back(StateRow(PERIOD, _TOKEN_END_STATE));
+    _states_vec.push_back(State(31, v31));
+
+    /* 40: colon first: we are looking at : or := tokens */
+    std::vector<StateRow> v40;
+    v40.push_back(StateRow(EQUALS, _TOKEN_END_STATE));
+    v40.push_back(StateRow(NOT_ANY, _TOKEN_BREAK_STATE, 1));
+    _states_vec.push_back(State(40, v40));
+
+    /* 50: quote mark: string processing */
+    std::vector<StateRow> v50;
+    v50.push_back(StateRow(QUOTEM, _TOKEN_END_STATE));
+    v50.push_back(StateRow(BACKSLASH, 51));
+    v50.push_back(StateRow(QUOTEM, 50, 1)); /* not quote mark */
+    _states_vec.push_back(State(50, v50));
+    std::vector<StateRow> v51;
+    v51.push_back(StateRow(NOT_ANY, 50, 1)); /* not not any => any */
+    _states_vec.push_back(State(51, v51));
+
 }
 
 Scanner::~Scanner()
@@ -60,13 +95,83 @@ int Scanner::is_digit(char c)
     return (strchr(digit_chars, c) != NULL);
 }
 
+Scanner::CHARTYPE Scanner::get_char_type(char c)
+{
+    if (is_alpha(c)) {
+        return ALPHA;
+    } else if (is_digit(c)) {
+        return DIGIT;
+    }
+    switch(c) {
+        case '.':
+            return PERIOD;
+        case ',':
+            return COMMA;
+        case ':':
+            return COLON;
+        case ';':
+            return SEMICOLON;
+        case ')':
+            return BRACKET_RIGHT;
+        case '(':
+            return BRACKET_LEFT;
+        case '+':
+            return PLUS;
+        case '-':
+            return MINUS;
+        case '/':
+            return SLASH;
+        case '\\':
+            return BACKSLASH;
+        case '!':
+            return EXCLAMATIONM;
+        case '*':
+            return ASTERISK;
+        case '&':
+            return AND;
+        case '"':
+            return QUOTEM;
+        case '=':
+            return EQUALS;
+        case '_':
+            return UNDERSCORE;
+        case ' ':
+            return SPACE;
+        case '\t':
+            return TAB;
+        case '\n':
+            return NEWLINE;
+        default:
+            return OTHER;
+    }
+}
+
+int Scanner::get_next_state(char next_char, int curr_state)
+{
+    std::vector<StateRow> vec = _states_vec[curr_state].state_rows;
+    CHARTYPE nc_type = get_char_type(next_char), ns_char_type = OTHER;
+    int ns_not_flag = 0;
+    for (std::vector<StateRow>::iterator it = vec.begin(); it != vec.end(); ++it) {
+        ns_char_type = (*it).next_char;
+        ns_not_flag = (*it).not_flag;
+        if ((nc_type == ns_char_type && !ns_not_flag) |
+            (nc_type != ns_char_type && ns_not_flag)) {
+            return (*it).next_state;
+        } 
+    }
+    /* returning error state because no defined state for this char */
+    return _TOKEN_ERROR_STATE;
+}
+
 Token Scanner::run_automaton(std::string *strbuffer)
 {
-    char curr_c = 0;
-    _input_file.get(curr_c);
+    _curr_state = 0;
+    char curr_c = 0, peek_c = 0;
+    peek_c = _input_file.peek();
     /* get rid of whitespace */
-    while(is_whitespace(curr_c) && strbuffer->size() == 0 && _input_file.good()) {
+    while(is_whitespace(peek_c) && strbuffer->size() == 0 && _input_file.good()) {
         _input_file.get(curr_c);
+        peek_c = _input_file.peek();
     }
     
     if (!_input_file.good()) {
@@ -75,7 +180,18 @@ Token Scanner::run_automaton(std::string *strbuffer)
 
     // TODO: filter comments away
 
-     
+    int next_state = _TOKEN_ERROR_STATE;
+    while (_curr_state >= 0) {
+        peek_c = _input_file.peek();
+        next_state = get_next_state(peek_c, _curr_state);
+        if (next_state != _TOKEN_ERROR_STATE) {
+            _input_file.get(curr_c);
+            strbuffer->push_back(curr_c);
+            _curr_state = next_state;
+        }
+    }    
+
+    // TODO: special case of strbuffer = 0 at this state.
  
     return create_token(*strbuffer);
 }
@@ -90,6 +206,11 @@ Token Scanner::create_token(std::string str)
 
     // TODO construct token from given string
     return Token(0, str);
+}
+
+Token Scanner::create_error_token(std::string str)
+{
+    return Token(Token::Error, str);
 }
 
 void Scanner::open_input_file(char *filename)
--- src/scanner.hpp
+++ src/scanner.hpp
@@ -18,12 +18,14 @@ private:
     /* chartypes for our state-machine's table-structure */
     enum CHARTYPE {ALPHA, DIGIT, PERIOD, COMMA, COLON, SEMICOLON, BRACKET_RIGHT,
         BRACKET_LEFT, PLUS, MINUS, SLASH, BACKSLASH, EXCLAMATIONM, ASTERISK, AND,
-        QUOTEM, EQUALS, SPACE, TAB, NEWLINE};
+        QUOTEM, EQUALS, UNDERSCORE, SPACE, TAB, NEWLINE, OTHER, NOT_ANY};
 
     struct StateRow {
         CHARTYPE next_char;
         int next_state;
-        StateRow(CHARTYPE nc, int ns) : next_char(nc), next_state(ns) { }
+        int not_flag;
+        StateRow(CHARTYPE nc, int ns) : next_char(nc), next_state(ns) { not_flag = 0; }
+        StateRow(CHARTYPE nc, int ns, int nf) : next_char(nc), next_state(ns), not_flag(nf) { }
     };
 
     struct State {
@@ -33,8 +35,9 @@ private:
     };
 
     /* negative integers to define static token construction states */
-    static const int _TOKEN_COMPLETE_STATE = -1;
-    static const int _TOKEN_ERROR_STATE = -2;
+    static const int _TOKEN_END_STATE = -1;
+    static const int _TOKEN_BREAK_STATE = -2;
+    static const int _TOKEN_ERROR_STATE = -3;
 
     std::vector<State> _states_vec;
     int _curr_state;
@@ -44,9 +47,12 @@ private:
     int is_whitespace(char c);
     int is_alpha(char c);
     int is_digit(char c);
+    CHARTYPE get_char_type(char c);
 
+    int get_next_state(char next_char, int curr_state);
     Token run_automaton(std::string *strbuffer);
     Token create_token(std::string str);
+    Token create_error_token(std::string str);
 public:
     Scanner();
     ~Scanner();
