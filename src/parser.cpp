f868be5d402921fd0fd342caccfb31631ac004d2
Leo Muona <leo.muona@gmail.com>
2014-03-11 15:05:24 +0200

    some initial stuff for parser

--- src/parser.cpp
+++ src/parser.cpp
@@ -2,5 +2,34 @@
 
 namespace mpli {
 
+void Parser::match(Token::TYPE expected)
+{
+	if (_curr_token.type == expected) {
+		get_next_token();
+	} else {
+		token_error();
+	}
+}
+
+void Parser::get_next_token()
+{
+	_curr_token = _scanner->next_token();
+}
+
+void Parser::token_error()
+{
+	// TODO: print error
+}
+
+void Parser::set_scanner(Scanner *scanner)
+{
+	_scanner = scanner;
+}
+
+void Parser::start()
+{
+	get_next_token();
+	// TODO
+}
 
 } // namespace mpli
--- src/parser.hpp
+++ src/parser.hpp
@@ -1,13 +1,23 @@
 #ifndef MPLI_PARSER_HPP_
 #define MPLI_PARSER_HPP_
 
+#include "token.hpp"
+#include "scanner.hpp"
+
 namespace mpli {
 
 class Parser {
     private:
+		Scanner *_scanner;
 
+		Token _curr_token;
+
+		void match(Token::TYPE expected);
+		void get_next_token();
+		void token_error();
     public:
-
+		void set_scanner(Scanner *scanner);
+		void start();
 };
 
 } // namespace mpli
